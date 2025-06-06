c20893b02792aab0dc0ca138b30aa9988006a190
panghudx <abb295390@gmail.com>
2025-06-06 21:53:50 +0800

    Update login.py

--- login.py
+++ login.py
@@ -28,14 +28,27 @@ CONSOLE_URL = "https://freecloud.ltd/member/index"
 RENEW_URL = f"https://freecloud.ltd/server/detail/{MACHINE_ID}/renew"
 
 # 公共请求头
-HEADERS = {
+# HEADERS = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+#                   "AppleWebKit/537.36 (KHTML, like Gecko) "
+#                   "Chrome/122.0.0.0 Safari/537.36",
+#     "Referer": "https://freecloud.ltd/login",
+#     "Origin": "https://freecloud.ltd",
+#     "Content-Type": "application/x-www-form-urlencoded"
+# }
+HEADERS=HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/122.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
     "Referer": "https://freecloud.ltd/login",
     "Origin": "https://freecloud.ltd",
     "Content-Type": "application/x-www-form-urlencoded"
 }
+
 
 # 登录表单数据
 LOGIN_PAYLOAD = {
